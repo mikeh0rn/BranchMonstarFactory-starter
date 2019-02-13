55ecb3d4e5074521917d38e8fce7b935fb2881c4
Mike Horn <mikelaurencehorn@gmail.com>
2019-02-13 18:28:11 -0500

    Initial Commit

---
+++ BranchMonstarFactory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BranchMonstarFactory
+//
+//  Created by Michael Horn on 2/13/19.
+//  Copyright © 2019 Mike Horn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
