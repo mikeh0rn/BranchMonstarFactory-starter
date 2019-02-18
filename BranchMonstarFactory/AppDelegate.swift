2e74b9631d726947094fd17527aa6f0b1cea6260
Mike Horn <mikelaurencehorn@gmail.com>
2019-02-17 23:09:08 -0500

    update app delegate and info plist

--- BranchMonstarFactory/AppDelegate.swift
+++ BranchMonstarFactory/AppDelegate.swift
@@ -24,19 +24,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        Branch.getInstance().application(app, open: url, options: options)
+//        Branch.getInstance().application(app, open: url, options: options)
+        
         return true
     }
     
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         // handler for Universal Links
-        Branch.getInstance().continue(userActivity)
+//        Branch.getInstance().continue(userActivity)
         return true
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        // handler for Push Notifications
-        Branch.getInstance().handlePushNotification(userInfo)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
